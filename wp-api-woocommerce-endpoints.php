f0f13373d0f487a75d1fc059d81d981fe8d1bebc
François-Xavier Wiplier <fx@wiplier.com>
2017-08-19 18:04:30 +0200

    init

--- wp-api-woocommerce-endpoints.php
+++ wp-api-woocommerce-endpoints.php
@@ -7,7 +7,36 @@ Version: 1.1
 Author URI: http://pmr.io
 */
 
+
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+add_action( 'rest_api_init', 'myplugin_add_karma' );
+function myplugin_add_karma() {
+    register_rest_field( 'comment', 'karma', array(
+        'get_callback' => function( $comment_arr ) {
+            $comment_obj = get_comment( $comment_arr['id'] );
+            return (int) $comment_obj->comment_karma;
+        },
+        'update_callback' => function( $karma, $comment_obj ) {
+            $ret = wp_update_comment( array(
+                'comment_ID'    => $comment_obj->comment_ID,
+                'comment_karma' => $karma
+            ) );
+            if ( false === $ret ) {
+                return new WP_Error(
+                    'rest_comment_karma_failed',
+                    __( 'Failed to update comment karma.' ),
+                    array( 'status' => 500 )
+                );
+            }
+            return true;
+        },
+        'schema' => array(
+            'description' => __( 'Comment karma.' ),
+            'type'        => 'integer'
+        ),
+    ) );
+} );
 
 /**
  * Check if WooCommerce is active
